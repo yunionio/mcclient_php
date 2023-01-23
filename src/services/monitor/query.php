34f779575e0ab85ea71b024fd70729bf674316e4
Qiu Jian <qiujian@yunionyun.com>
2023-01-23 13:02:17 +0800

    fix: add monitor query

---
+++ src/services/monitor/query.php
@@ -0,0 +1,176 @@
+<?php
+// Copyright 2019 Yunion
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+function monitor_signature($body) {
+    if (array_key_exists("signature", $body)) {
+        unset($body["signature"]);
+    }
+    ksort($body, SORT_STRING);
+    $jbody = json_encode($body, JSON_UNESCAPED_UNICODE);
+    $sig = hash('sha256', $jbody);
+    $body["signature"] = $sig;
+    return $body;
+}
+
+/*
+{
+  "metric_query": [
+    {
+      "model": {
+        "measurement": "vm_diskio",
+        "select": [
+          [
+            {
+              "type": "field",
+              "params": [
+                "read_bps"
+              ]
+            },
+            {
+              "type": "mean",
+              "params": []
+            },
+            {
+              "type": "alias",
+              "params": [
+                "磁盘读速率"
+              ]
+            }
+          ]
+        ],
+        "tags": [
+          {
+            "key": "vm_id",
+            "value": "ab9502de-c6b6-4150-880b-d0e3e6ba8ec8",
+            "operator": "="
+          }
+        ],
+        "group_by": [
+          {
+            "type": "tag",
+            "params": [
+              "vm_id"
+            ]
+          }
+        ]
+      }
+    }
+  ],
+  "scope": "system",
+  "from": "24h",
+  "interval": "5m",
+  "unit": false,
+  "signature": "5ad57b46104d4a2daad323df4d87bcb22394d9b977acb4094089eb9d941c65c4"
+}
+*/
+class MetricQuery {
+    private $id_key;
+    private $id_value;
+    private $measurement;
+    private $field;
+    private $alias;
+
+    public function __construct($measurement, $field, $alias, $idkey, $idvalue) {
+        $this->id_key = $idkey;
+        $this->id_value = $idvalue;
+        $this->measurement = $measurement;
+        $this->field = $field;
+        $this->alias = $alias;
+    }
+
+    function query() {
+        return array(
+            "measurement"=>$this->measurement,
+            "select"=>array(
+                array(
+                    array(
+                        "type"=>"field",
+                        "params"=>array($this->field),
+                    ),
+                    array(
+                        "type"=>"mean",
+                        "params"=>array(),
+                    ),
+                    array(
+                        "type"=>"alias",
+                        "params"=>array($this->alias),
+                    ),
+                ),
+            ),
+            "tags"=>array(
+                array(
+                    "key"=>$this->id_key,
+                    "value"=>$this->id_value,
+                    "operator"=>"=",
+                ),
+            ),
+            "group_by"=>array(
+                array(
+                    "type"=>"tag",
+                    "params"=>array($this->id_key),
+                ),
+            ),
+        );
+    }
+}
+
+class Query {
+    private $from;
+    private $interval;
+    private $scope;
+    private $unit;
+    private $metrics;
+
+    public function __construct($from, $interval) {
+        $this->from = $from;
+        $this->interval = $interval;
+        $this->scope = "system";
+        $this->unit = false; 
+        $this->metrics = array();      
+    }
+
+    function add_metric($measurement, $field, $alias, $idkey, $idvalue) {
+        array_push($this->metrics, new MetricQuery($measurement, $field, $alias, $idkey, $idvalue));
+        return $this;
+    }
+
+    function query() {
+        $metrics = array();
+        for($i=0;$i<count($this->metrics);$i++) {
+            array_push($metrics, array("model"=>$this->metrics[$i]->query()));
+        }
+        $q = array(
+            "metric_query"=>$metrics,
+            "scope"=>$this->scope,
+            "from"=>$this->from,
+            "interval"=>$this->interval,
+            "unit"=>$this->unit,
+        );
+        return monitor_signature($q);
+    }
+}
+
+if (!debug_backtrace()) {
+    $q = new Query("24h", "5m");
+    $q->add_metric("vm_diskio", "read_bps", "磁盘读速率", "vm_id", "ab9502de-c6b6-4150-880b-d0e3e6ba8ec8");
+    print(json_encode($q->query()));
+
+    /*$mon = new UnifiedMonitorManager();
+    $params = $q->query();
+    print_r($mon->query($s, $params));
+    */
+}
+
+?>
